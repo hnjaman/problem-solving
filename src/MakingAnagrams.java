07bf071db307decf33f49eb335b93bf4a7f1b140
Hasan Noor Jaman <hnjsujon.mbstu@gmail.com>
2018-02-21 14:21:35 +0600

    [ not solved ]

--- src/MakingAnagrams.java
+++ src/MakingAnagrams.java
@@ -1,20 +1,26 @@
+
 import java.util.Scanner;
 
 public class MakingAnagrams {
     public static int numberNeeded(String first, String second) {
-//        StringBuilder sf = new StringBuilder(first);
-//        StringBuilder ss = new StringBuilder(second);
-//        System.out.println(sf);
-//        System.out.println(ss);
+        StringBuilder sf = new StringBuilder(first);
+        StringBuilder ss = new StringBuilder(second);
+        //System.out.println(sf);
+       // System.out.println(ss);
         int count = 0;
-        for(int i=0;i<first.length();i++){
-            for(int j = 0; j<second.length();j++){
-                if(first.charAt(i)==second.charAt(j)){
+        for(int i=0;i<sf.length();i++){
+            for(int j = 0; j<ss.length();j++){
+                if(sf.charAt(i)==ss.charAt(j)){
+                    sf.deleteCharAt(i);
+                    ss.deleteCharAt(j);
                     count=count+2;
+                    break;
                 }
             }
         }
-        return first.length()+second.length()-count;
+        System.out.println(sf);
+        System.out.println(ss);
+        return sf.length()+ss.length();
     }
 
     public static void main(String[] args) {
