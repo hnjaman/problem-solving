3a96a24d2f771bd7186bab686ece73b64b7da4db
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-07-23 18:00:33 +0600

    java hack

---
+++ src/RansomNote.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class RansomNote {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] mn = scanner.nextLine().split(" ");
+        int m = Integer.parseInt(mn[0]);
+        int n = Integer.parseInt(mn[1]);
+
+        String[] magazine = new String[m];
+
+        String[] magazineItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            String magazineItem = magazineItems[i];
+            magazine[i] = magazineItem;
+        }
+
+        String[] ransom = new String[n];
+
+        String[] ransomItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            String ransomItem = ransomItems[i];
+            ransom[i] = ransomItem;
+        }
+
+        scanner.close();
+    }
+}
---
+++ src/hackjava/OutputFormatting.java
@@ -0,0 +1,31 @@
+package hackjava;
+
+import java.util.Scanner;
+
+public class OutputFormatting {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //StringBuilder xs = new StringBuilder(x);
+//            String xs=new String(String.valueOf(x));
+//            String xss="";
+//            if(xs.length() == 1){
+//                 xss="00"+xs;
+//            }else if(xs.length()==2){
+//                 xss="0"+xs;
+//            }else{
+//                xss=xs;
+//            }
+//           // String xss =xs.length()>2 ? String.valueOf(xs) : "0"+xs;
+//            //System.out.println(xss);
+//
+//            System.out.println(s1+"\t\t\t"+xss);
+            System.out.printf("%-15s%03d%n", s1, x);
+        }
+        System.out.println("================================");
+
+    }
+}
