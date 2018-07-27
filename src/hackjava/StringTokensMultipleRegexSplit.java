e20658768cd9a76730efe462f0e984f27f31b1e7
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-07-27 12:24:18 +0600

    [solved] pattern checker & multiple delimiter split

--- src/hackjava/StringTokensMultipleRegexSplit.java
+++ src/hackjava/StringTokensMultipleRegexSplit.java
@@ -6,10 +6,19 @@ public class StringTokensMultipleRegexSplit {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
-       // String ss=s.trim();
-        String[] sp=s.split("_|\\.|\\s|\\,|\\!|\\,|\\?|\\'|\\@");
-        for(int i=0;i<sp.length;i++){
-            System.out.println(sp[i]);
+        String ss=s.trim();
+       // String[] sp=s.split("_|\\.|\\s|\\,|\\!|\\,|\\?|\\'|\\@");
+        int len=ss.length();
+        int count=0;
+        if(len>0) {
+            String[] sp = ss.split("[-. _,!?'@]+");
+            System.out.println(sp.length);
+            for (int i = 0; i < sp.length; i++) {
+                // count=count+sp[i].length();
+                System.out.println(sp[i]);
+            }
+        }else{
+            System.out.println(len);
         }
         scan.close();
     }
