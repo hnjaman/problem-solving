7b940f135e2d0de98dcef8a2363fbd2e9cb5777b
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-07-28 18:17:10 +0600

    [solved] java list

---
+++ src/hackjava/JavaList.java
@@ -0,0 +1,37 @@
+package hackjava;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class JavaList {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        List<Integer> l = new ArrayList<>(n);
+        for (int i = 1; i <= n; i++) {
+            l.add(in.nextInt());
+        }
+
+        int q = in.nextInt();
+        in.nextLine();
+
+        while (q-- != 0) {
+            int index;
+            String s = in.next();
+            if (s.equals("Insert")) {
+                index = in.nextInt();
+                int value = in.nextInt();
+                l.add(index, value);
+            }
+            if (s.equals("Delete")) {
+                index = in.nextInt();
+                l.remove(index);
+            }
+        }
+
+        for (Integer e: l) {
+            System.out.print(e+" ");
+        }
+    }
+}
