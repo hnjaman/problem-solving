ad341b6f2d5027ddeaed8ef5a220b1679e9c89c3
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-07-29 18:54:06 +0600

    [not solved] java map

---
+++ src/hackjava/JavaMap.java
@@ -0,0 +1,22 @@
+package hackjava;
+
+import java.util.Scanner;
+
+public class JavaMap {
+    public static void main(String []argh)
+    {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.nextLine();
+        for(int i=0;i<n;i++)
+        {
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            in.nextLine();
+        }
+        while(in.hasNext())
+        {
+            String s=in.nextLine();
+        }
+    }
+}
