773cad82ed94e5d4ccc82091013fcba64962de13
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-07-30 17:30:43 +0600

    [solved] a very big sum

--- src/ProblemSolving/ComparetheTriplets.java
+++ src/ProblemSolving/ComparetheTriplets.java
@@ -12,7 +12,7 @@ public class ComparetheTriplets {
     static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
             int alice=0;
             int bob=0;
-           
+
        for(int i=0;i<a.size();i++){
            if(a.get(i)>b.get(i)){
                alice++;
