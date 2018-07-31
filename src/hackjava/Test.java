9d9d282ae29edb5261e753b7d9a9094f2d7a9350
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-07-31 16:46:56 +0600

    [solved] time conversion

--- src/hackjava/Test.java
+++ src/hackjava/Test.java
@@ -6,12 +6,13 @@ import java.util.regex.Pattern;
 
 public class Test {
     public static void main(String args[]){
-       String s="-100";
-       String s2="50";
-        BigDecimal a=new BigDecimal(s);
-        System.out.println(a);
-        a=new BigDecimal(s2);
-        System.out.println(a);
+        int aa=02;
+//       String s="010";
+//       String s2="50";
+//        BigDecimal a=new BigDecimal(s);
+//        System.out.println(a);
+//        a=new BigDecimal(s2);
+        System.out.println(10-aa);
 //        if(b!=a) {
 //            System.out.println("n");//false ($ is not matched)
 //        }else {
