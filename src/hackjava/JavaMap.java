23bb51d82c9700a7214cc116a62ba5060b9a8376
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-08-01 18:28:59 +0600

    [not solved due to time limit] java map

--- src/hackjava/JavaMap.java
+++ src/hackjava/JavaMap.java
@@ -1,11 +1,14 @@
 package hackjava;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class JavaMap {
-    public static void main(String []argh)
+    public static void main(String []arg)
     {
         Scanner in = new Scanner(System.in);
+        Map<String,Integer> phonebook=new HashMap<>();
         int n=in.nextInt();
         in.nextLine();
         for(int i=0;i<n;i++)
@@ -13,10 +16,30 @@ public class JavaMap {
             String name=in.nextLine();
             int phone=in.nextInt();
             in.nextLine();
+            phonebook.put(name,phone);
         }
-        while(in.hasNext())
-        {
+        while(in.hasNext()) {
             String s=in.nextLine();
+            try{
+                int num=phonebook.get(s);
+                System.out.println(s+"="+num);
+            }catch (NullPointerException e){
+                System.out.println("Not found");
+            }
         }
+
+//        for(int i=0;i<n;i++){
+//            String s=in.nextLine();
+//            int count=0;
+//            for (Map.Entry<String,Integer> entry: phonebook.entrySet()) {
+//                if(entry.getKey().equals(s)){
+//                    count=1;
+//                    System.out.println(entry.getKey()+"="+entry.getValue());
+//                }
+//            }
+//            if(count==0){
+//                System.out.println("Not found");
+//            }
+//        }
     }
 }
