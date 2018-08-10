6409f4bdaf1ac157cc9e5d05ff6b2078f0114609
HasanNoorJamanSujon <hnjsujon.mbstu@gmail.com>
2018-08-10 14:06:48 +0600

    [solved] java hashset

--- src/hackjava/JavaHashset.java
+++ src/hackjava/JavaHashset.java
@@ -20,16 +20,8 @@ public class JavaHashset {
         }
 
         for (int i = 0; i < t; i++) {
-            left.add(pair_left[i]);
-            right.add(pair_right[i]);
-            if(left.size()>right.size()){
-                System.out.println(left.size());
-            }else if(right.size()>left.size()){
-                System.out.println(right.size());
-            }else{
-                System.out.println(right.size());
-            }
-            //System.out.println(left.size()+" "+right.size());
+            left.add("(" + pair_left[i] + ", " + pair_right[i] + ")" );
+            System.out.println(left.size());
         }
     }
 }
