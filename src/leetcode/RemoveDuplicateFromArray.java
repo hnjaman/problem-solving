5c240dbe6feb2b5992ab411ce3e290191a3ccc92
hnjaman <hasan.jaman@bjitgroup.com>
2019-08-02 12:59:06 +0600

    [not solved] remove element

--- src/leetcode/RemoveDuplicateFromArray.java
+++ src/leetcode/RemoveDuplicateFromArray.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 public class RemoveDuplicateFromArray {
     public static void main(String[] args) {
         int[] array = new int[10];
-        array = new int[]{1, 1, 3, 5, 5, 5};
+        array = new int[]{1, 1, 2};
        // System.out.println(array.length);
         System.out.println(removeDuplicate(array));
     }
 
     private static int removeDuplicate(int[] array) {
-        if(array.length==0)
-            return 0;
+        if(array.length==0 || array.length==1)
+            return array.length;
         int i=0;
         for(int j=0; j<array.length; j++){
             if(array[j] != array[i]){
@@ -20,6 +20,6 @@ public class RemoveDuplicateFromArray {
                 array[i]=array[j];
             }
         }
-        return array.length;
+        return i+1;
     }
 }
---
+++ src/leetcode/RemoveElements.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class RemoveElements {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        array = new int[]{0,1,2,2,3,0,4,2};
+        // System.out.println(array.length);
+        System.out.println(removeElement(array, 2));
+    }
+
+    private static int removeElement(int[] array, int element) {
+//        if(array.length==0 || array.length==1)
+//            return array.length;
+        int k=0;
+        for(int i=0; i<array.length; i++){
+            for(int j=i; j<array.length; j++){
+                if(array[i] == element && j==array.length-1){
+                    array[i]=-1;
+                    k++;
+                    return array.length-k;
+                }else if(array[i] == element && j!=array.length-1){
+                    array[i]=array[j+1];
+                    k++;
+                }
+            }
+        }
+        return array.length-k;
+    }
+}
