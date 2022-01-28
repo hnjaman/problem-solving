44e73822a152eb1e9e3b7c37e6310dc34cc0d4ed
Hasan <hasan.jaman@ejogajogbd.com>
2022-01-28 16:59:35 +0600

    amazon pblm solve

---
+++ code2022/RemoveNumbersFromString.java
@@ -0,0 +1,10 @@
+package code2022;
+
+/**
+ * Created by Lenovo on 26/01/2022.
+ */
+public class RemoveNumbersFromString {
+    public static void main(String[] args) {
+        String string = "";
+    }
+}
--- code2022/amazonHackerRank/MaximumPageViews.java
+++ code2022/amazonHackerRank/MaximumPageViews.java
@@ -64,19 +64,135 @@ class Result {
 
     public static int maximumPages(SinglyLinkedListNode head) {
         int max = 0;
-        int current = head.data;
-        int previousCurrent;
-        head = head.next;
-        while (head != null && head.data>= 0) {
-            if(head.next == null && head.data>= 0) {
-                previousCurrent = current;
-                current = head.data;
-                head.data = -1;
-            } else {
-                head = head.next;
+        int first = 0;
+        int last = 0;
+        SinglyLinkedListNode copyHead;
+        SinglyLinkedListNode headBackup;
+
+            copyHead = head;
+            first = head.data;
+            headBackup = head;
+            while (copyHead != null){
+                if(copyHead.next == null && copyHead.data >= 0){
+                    last = copyHead.data;
+                    copyHead.data = Integer.MIN_VALUE;
+                    break;
+                } else if(copyHead.data > 0){
+                    if(copyHead.next.data < 0) {
+                        last = copyHead.data;
+                        copyHead.data = Integer.MIN_VALUE;
+                    }
+                }
+                copyHead = copyHead.next;
+                if(first+last > max){
+                    max = head.data + last;
+                }
             }
-        }
+
+            head = headBackup;
+
         return max;
+    }
+
+//    My solve *************************
+
+//    public static int maximumPages(SinglyLinkedListNode head) {
+//        int max = 0;
+//        int last = 0;
+//        SinglyLinkedListNode copyHead;
+//        SinglyLinkedListNode headBackup;
+//
+//        while (head != null) {
+//            copyHead = head.next;
+//            headBackup = head.next;
+//            while (copyHead != null){
+//                if(copyHead.next == null && copyHead.data >= 0){
+//                    last = copyHead.data;
+//                    copyHead.data = Integer.MIN_VALUE;
+//                    break;
+//                } else if(copyHead.data > 0){
+//                    if(copyHead.next.data < 0) {
+//                        last = copyHead.data;
+//                        copyHead.data = Integer.MIN_VALUE;
+//                    }
+//                }
+//                copyHead = copyHead.next;
+//            }
+//            if(head.data+last > max){
+//                max = head.data + last;
+//            }
+//
+//            head = headBackup;
+//        }
+//        return max;
+//    }
+
+    public static int pairSum(SinglyLinkedListNode head) {
+        SinglyLinkedListNode middle = getMiddleNode(head);
+
+        SinglyLinkedListNode secondHead = middle.next;
+        middle.next = null;
+
+        SinglyLinkedListNode firstHead = reverse(head);
+
+        return findMaxSum(firstHead, secondHead);
+    }
+
+    public static SinglyLinkedListNode getMiddleNode(SinglyLinkedListNode head) {
+        int size = getSize(head);
+
+        SinglyLinkedListNode current = head;
+        for (int index = 0; index < (size / 2) - 1; index++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    public static int getSize(SinglyLinkedListNode head) {
+        int size = 0;
+        SinglyLinkedListNode current = head;
+        while (current != null) {
+            size++;
+            current = current.next;
+        }
+        return size;
+    }
+
+    public static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
+        SinglyLinkedListNode previous = null;
+        SinglyLinkedListNode current = head;
+        while (current != null) {
+            SinglyLinkedListNode temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+        return previous;
+    }
+
+    public static int findMaxSum(SinglyLinkedListNode firstHead, SinglyLinkedListNode secondHead) {
+        int maxSum = Integer.MIN_VALUE;
+        while (firstHead != null) {
+            maxSum = Math.max(maxSum, firstHead.data + secondHead.data);
+            firstHead = firstHead.next;
+            secondHead = secondHead.next;
+        }
+
+        return maxSum;
+    }
+
+    public static SinglyLinkedListNode reverse2(SinglyLinkedListNode node) {
+        SinglyLinkedListNode prev = null;
+        SinglyLinkedListNode current = node;
+        SinglyLinkedListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
     }
 
 }
@@ -97,6 +213,7 @@ public class MaximumPageViews {
         }
 
         int result = Result.maximumPages(head.head);
+//        int result = Result.pairSum(head.head);
         scanner.close();
         System.out.println(result);
     }
@@ -107,16 +224,16 @@ public class MaximumPageViews {
 //    int max = 0;
 //    ArrayList<Integer> list = new ArrayList<>();
 //        while (head != null){
-//                list.add(head.data);
-//                head = head.next;
-//                }
-//                int length = list.size()/2;
-//                for(int i=0; i<length; i++){
-//        if(max<list.get(i)+list.get(list.size()-(i+1)))
-//        max = list.get(i)+list.get(list.size()-(i+1));
+//          list.add(head.data);
+//          head = head.next;
+//        }
+//        int length = list.size()/2;
+//        for(int i=0; i<length; i++){
+//          if(max<list.get(i)+list.get(list.size()-(i+1)))
+//          max = list.get(i)+list.get(list.size()-(i+1));
 //        }
 //
-//        return max;
+//    return max;
 
 
 //        while (head.next != null){
